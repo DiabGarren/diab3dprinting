69246ee3b7c453d88bbe82e59bd776aee6165f58
DiabGarren <garrendiab@gmail.com>
2025-07-28 12:33:03 +0200

    Update user info

--- app/api/user/[id]/route.tsx
+++ app/api/user/[id]/route.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import connectDb from "@/lib/connectDb";
 import { createErrorResponse } from "@/lib/utils";
 import User from "@/models/user";
@@ -23,7 +24,57 @@ export async function GET(
             status: 201,
             headers: { "Content-Type": "application/json" },
         });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        return createErrorResponse(error.message, 500);
+    }
+}
+
+export async function PUT(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        await connectDb();
+
+        const { id } = await params;
+        const user = await User.findOne({ _id: id });
+
+        if (!user) {
+            return createErrorResponse("No user found", 404);
+        }
+
+        const body = await request.json();
+
+        const newUser = await User.updateOne(
+            { _id: id },
+            {
+                firstName: body.firstName,
+                lastName: body.lastName,
+                username: body.username.toLowerCase(),
+                email: body.email.toLowerCase(),
+                phone: body.phone,
+                prefer: body.prefer,
+                password: body.password,
+                level: body.level,
+                cart: body.cart,
+                address: {
+                    street: body.address.street,
+                    suburb: body.address.suburb,
+                    city: body.address.city,
+                    postalCode: body.address.postalCode,
+                },
+            }
+        );
+
+        const res = {
+            status: "success",
+            data: newUser,
+        };
+
+        return new NextResponse(JSON.stringify(res), {
+            status: 201,
+            headers: { "Content-Type": "application/json" },
+        });
     } catch (error: any) {
         return createErrorResponse(error.message, 500);
     }
