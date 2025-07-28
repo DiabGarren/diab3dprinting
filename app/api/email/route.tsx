072451a7d51ca28150819c86b6891419d90e39cc
DiabGarren <garrendiab@gmail.com>
2025-07-28 12:27:14 +0200

    General email api

---
+++ app/api/email/route.tsx
@@ -0,0 +1,27 @@
+import { sendMail } from "@/components/email";
+import { createErrorResponse } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const info = sendMail({
+            sendTo: body.sendTo,
+            body: body.body,
+            html: body.html,
+            subject: body.subject,
+        });
+
+        const res = {
+            status: "success",
+            data: info,
+        };
+        return new NextResponse(JSON.stringify(res), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        return createErrorResponse(error.message, 500);
+    }
+}
---
+++ components/email.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+
+export async function sendMail({
+    email = process.env.GMAIL,
+    sendTo,
+    subject,
+    body,
+    html,
+}: {
+    email?: string;
+    sendTo?: string;
+    subject?: string;
+    body?: string;
+    html?: string;
+}) {
+    try {
+        await transporter.verify();
+    } catch (error: any) {
+        console.error("Something went wrong");
+        return;
+    }
+
+    const info = await transporter.sendMail({
+        from: email,
+        to: sendTo,
+        bcc: email,
+        subject: subject,
+        text: body,
+        html: html,
+    });
+
+    console.log("Mail sent", info.messageId);
+    return info;
+}
